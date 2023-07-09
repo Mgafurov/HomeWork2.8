714c06363d05bf4a25c037fe3823dac05d9ea6c2
Malik <poseydon100500@gmail.com>
2023-07-09 23:41:23 +0500

    Добавьте файлы проекта.

---
+++ HomeWork2.8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class HomeWork
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение x : ");
+        float x = float.Parse(Console.ReadLine());
+        Console.WriteLine("Введите значение переменной y : ");
+        float y = float.Parse(Console.ReadLine());
+        float result = (x + y) / (y + 1) - ((x * y - 12) / (34 + x));
+        Console.WriteLine("Результат : " + result);
+        Console.ReadKey();
+    }
+}
